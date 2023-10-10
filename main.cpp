a696002284b4d3d7d72880d1d7256cc00434d3a2
chrsBronson <yzharko@ya.ru>
2023-10-10 20:36:51 +0300

    user input slogan has benn added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
   std::cout << "This is the begining of the cpp Empire!\n";
+  std::cout << "What is our slogan will be, dear comrade?\n";
+
+  std::string inputStr = "";
+  std::getline(std::cin, inputStr);
+  std::cout << inputStr + " Indeed.\n";
 }
